71ce5f7a2903c382b652cd6a25f0dc33d3a78969
Ognjen <opejovic@gmail.com>
2020-06-08 23:15:58 +0200

    new design

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,9 +5,9 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=La+Belle+Aurore&display=swap');
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -15,6 +15,7 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  background-color: #1d1d1d;
 }
 
 *,
