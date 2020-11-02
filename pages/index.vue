c9919956b65f317cab2fce72075c36f3cd2397a2
Ognjen <opejovic@gmail.com>
2020-11-02 22:59:39 +0100

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -78,7 +78,7 @@
           <div
             id="animateFirstMobile"
             data-animate-in="up"
-            class="flex md:hidden text-6xl tracking-tighter font-alliance-medium text-teal-400"
+            class="flex md:hidden text-6xl tracking-tighter font-alliance-medium text-gradient-purple"
           >
             <h1>
               <p>Hi.</p>
